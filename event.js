a34eea452a07299aee5e0f609f57228a23998794
armannd092 <>
2021-04-23 12:24:37 +0200

    add remote asset new marker test

--- event.js
+++ event.js
@@ -1,14 +1,15 @@
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
-        const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#animated-model");
+        const animatedMarker = document.querySelector("#memarker");
+        const aEntity = document.querySelector("#univers");
+        console.log("inside!");
 
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-              console.log()
+              console.log(aEntity.value);
             }
         });
 }});
